9b2af8d23437e5dc62ddc39e132b4a7022ec7024
Denis CCK <2627434+denis-cck@users.noreply.github.com>
2025-12-09 14:23:37 +0100

    FIX: Used dotenv to load the environment variables in config

--- src/openaq_anomaly_prediction/config/config.py
+++ src/openaq_anomaly_prediction/config/config.py
@@ -3,28 +3,24 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+_ROOT_PATH = Path(__file__).parent.parent.parent.parent
+load_dotenv(dotenv_path=_ROOT_PATH / "secrets" / ".env")
+
 
 class Configuration:
-    ROOT_PATH = Path(__file__).parent.parent.parent.parent
+    # Root path
+    ROOT_PATH = _ROOT_PATH
 
     # Data paths
     DATA_PATH = ROOT_PATH / "data"
-
-    VERSION = "0.5"
-
-    # DEPRECATED: We're using MLflow for experiment tracking and model registration.
-    # Model paths
-    # Results and logging
-    # Model parameters
 
     # TODO: Load the environment variables in the class
     @staticmethod
     def getenv(var_name: str) -> str:
         """Get environment variable by name."""
-        return os.getenv(var_name, "")
+        return os.environ.get(var_name, "default")
 
     def __init__(self) -> None:
         pass
-
-
-# config = Configuration()
