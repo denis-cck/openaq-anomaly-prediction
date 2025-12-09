edf649130481a327fce9bf6558fef53e55ba4da5
Denis CCK <2627434+denis-cck@users.noreply.github.com>
2025-12-09 12:41:39 +0100

    FEAT: Modified the initial README.md

--- src/openaq_anomaly_prediction/config/config.py
+++ src/openaq_anomaly_prediction/config/config.py
@@ -1,5 +1,6 @@
 """Module for dynamic configuration variables of the project."""
 
+import os
 from pathlib import Path
 
 
@@ -9,12 +10,18 @@ class Configuration:
     # Data paths
     DATA_PATH = ROOT_PATH / "data"
 
+    VERSION = "0.5"
+
     # DEPRECATED: We're using MLflow for experiment tracking and model registration.
     # Model paths
     # Results and logging
     # Model parameters
 
     # TODO: Load the environment variables in the class
+    @staticmethod
+    def getenv(var_name: str) -> str:
+        """Get environment variable by name."""
+        return os.getenv(var_name, "")
 
     def __init__(self) -> None:
         pass
